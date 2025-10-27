e38d5771ac93ddb0682e3cd78eef7c563e90e342
jacekroszak <jacekroszak1986@gmail.com>
2025-10-27 12:52:15 +0100

    basic authorization and testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :authenticate_user!
+
+  check_authorization unless: :devise_controller?
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: exception.message
+  end
 end
---
+++ spec/support/capybara.rb
@@ -0,0 +1,27 @@
+require 'capybara/rspec'
+require 'selenium-webdriver'
+
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--window-size=1400,1400')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :headless_chrome
+Capybara.default_driver = :rack_test
+Capybara.app_host = 'http://localhost:3000'
+
+RSpec.configure do |config|
+  config.before(:each, type: :feature) do
+    if Capybara.current_driver == :rack_test
+      driven_by(:rack_test)
+    else
+      driven_by(:headless_chrome)
+    end
+  end
+end
